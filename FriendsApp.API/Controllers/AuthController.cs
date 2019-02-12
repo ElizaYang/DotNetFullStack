b7dd8a31c0cd4bf6a7ddd3aca37407f64d750f60
ElizaYang <32000035+ElizaYang@users.noreply.github.com>
2019-02-12 17:02:04 -0600

    add authentication and Token

--- FriendsApp.API/Controllers/AuthController.cs
+++ FriendsApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using FriendsApp.API.Data;
 using FriendsApp.API.Dtos;
 using FriendsApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FriendsApp.API.Controllers
 {
@@ -11,8 +17,10 @@ namespace FriendsApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config) // this is called injection
         {
+            _config = config;
             this._repo = repo;
         }
         [HttpGet]
@@ -26,7 +34,7 @@ namespace FriendsApp.API.Controllers
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
             // validation request
-            
+
             // get username to lowercase
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
             if (await _repo.UserExists(userForRegisterDto.Username))
@@ -37,8 +45,45 @@ namespace FriendsApp.API.Controllers
             };
 
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
-            
+
             return StatusCode(201); // http 201: created
         }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+            {
+                return Unauthorized();
+            }
+
+            // build token
+            // 1. retrive user id, username form db
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+            // generate sysmmetric key object using pre-config Token
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSetting:Token").Value));
+            // generate creds object using key
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+            // generate descriptor
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            // return a token object to reponse
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
+        }
     }
-}
+} 
