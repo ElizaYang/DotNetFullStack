23f5b85691c0278d2d800de665e659aa83224343
ElizaYang <32000035+ElizaYang@users.noreply.github.com>
2019-02-12 13:20:39 -0600

    add model validation

--- FriendsApp.API/Dtos/UserForRegisterDto.cs
+++ FriendsApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FriendsApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required] // can't be nullor empty, DataAnnotation valication
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Password length must between 4 and 8.")]
         public string Password { get; set; }
     }
 }
