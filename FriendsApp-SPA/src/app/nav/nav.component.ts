4767a09c42352399666c7b63973b9121fe2ba0e2
ElizaYang <32000035+ElizaYang@users.noreply.github.com>
2019-02-13 22:23:18 -0600

    create and inject authService

---
+++ FriendsApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {};
+  // "model" is a field name of this class, blined in nav.html
+  // inject AuthService to the constructor
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in');
+    }, error => {
+      console.log('Failed to login, some error');
+    });
+  }
+}
